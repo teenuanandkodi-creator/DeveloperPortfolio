e010dbee6a903de37b715bc9258394916d05f948
TeenuAnand Kodi <teenuanandkodi@gmail.com>
2026-07-28 13:18:47 +0200

    Add multiple pages and navigation

--- 11-Web-Development/Employee-Leave-Management-System/app.py
+++ 11-Web-Development/Employee-Leave-Management-System/app.py
@@ -8,5 +8,20 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/employees")
+def employees():
+    return render_template("employees.html")
+
+
+@app.route("/leave")
+def leave():
+    return render_template("leave.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
